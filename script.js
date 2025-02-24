a21774140540740af8bfd4552e08f2a2e11ab847
alex-abm <166135457+alex-abm@users.noreply.github.com>
2025-02-24 22:35:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const colorButton = document.getElementById('colorButton');
+colorButton.addEventListener('click', () => {
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+});
